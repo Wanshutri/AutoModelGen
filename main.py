ade2e3ab17d28bda1cca54bd853a6a0ce6ae77e2
Wanshutri <valenzuelavivancofelipe@gmail.com>
2023-09-25 13:11:51 -0300

    arreglar las descargas de ffmpeg

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import ctypes
 import sys
-from colorama import Fore, Style
+
 
 def is_admin():
     try:
@@ -14,18 +14,19 @@ os.system('cls')
 
 #Installation of requried libraries
 print("Installing required libraries")
-required_libraries = ["librosa", "pydub", "requests"]
+required_libraries = ["colorama", "pydub", "requests"]
 act_req = ""
 
 try:
+    
     for req in required_libraries:
         actReq = req
         subprocess.check_call(["pip", "install", req])
 except Exception as e:
-    print(f"{Fore.RED}Can't install the library {actReq}: \nError {e}{Style.RESET_ALL}")
+    print(f"Can't install the library {actReq}: \nError {e}")
     input()
     exit(0)
-
+from colorama import Fore, Style
 import os
 import zipfile
 import platform
@@ -41,13 +42,67 @@ else:
 
 import requests
 
-# URL de descarga del archivo público compartido en Google Drive
-ffmpeg_compressed_url = "https://drive.google.com/file/d/1YvDKWBGg4KBkKkrrQ220Etm_0h3V812j/view?usp=drive_link"
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download&confirm=1"
 
-# Ruta de destino para guardar el archivo descargado
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+if len(sys.argv) >= 3:
+        file_id = sys.argv[1]
+        destination = sys.argv[2]
+else:
+    try:
+        file_id = "1YvDKWBGg4KBkKkrrQ220Etm_0h3V812j"
+        destination = os.getcwd()
+        print(f"dowload {file_id} to {destination}")
+        download_file_from_google_drive(file_id, destination)
+    except PermissionError:
+        if is_admin():
+            print(f"{Fore.RED}{e}, , try moving the file to another path{Style.RESET_ALL}")
+            input()
+            exit(0)
+        else:
+            print("No admin privileges, restarting with privileges")
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            exit(0)
+    except Exception as e:
+            print(f"{Fore.RED}{e}{Style.RESET_ALL}")
+            input()
+            exit(0)
+
+'''
+# downlad URL for the public file from google drive
+#ffmpeg_compressed_url = "https://drive.google.com/file/d/1YvDKWBGg4KBkKkrrQ220Etm_0h3V812j/view?usp=drive_link"
+ffmpeg_compressed_url = "https://drive.google.com/u/0/uc?id=1YvDKWBGg4KBkKkrrQ220Etm_0h3V812j&export=download"
+# Path to save downloaded file
 output_path = os.getcwd()
 
-# Realizar la solicitud de descarga
+# Make request to download
 response = requests.get(ffmpeg_compressed_url)
 
 # Verificar si la descarga se completó correctamente
@@ -74,7 +129,7 @@ else:
     print("Can't download ffmpeg.")
     input()
     exit(0)
-
+'''
 # Installing ffmpeg
 
 ffmpeg_dir = os.path.join(os.getcwd(), "ffmpeg")
